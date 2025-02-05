e84facfbc9026fa0e16c5df463669e6d34e974af
Nguyễn Anh Tuấn <tuanna@hanu.edu.vn>
2025-02-05 17:39:40 +0700

    Upload via Desktop

---
+++ deploy_wordpress.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ask for password securely
+read -s -p "Enter decryption password: " SCRIPT_PASS
+echo
+
+# Download and decrypt the encrypted script, then execute it
+curl -s "https://raw.githubusercontent.com/tuanna-git/public/main/deploy_wordpress.sh.enc" | openssl enc -aes-256-cbc -d -pbkdf2 -salt -pass pass:"$SCRIPT_PASS" | bash
