1d4ae4370f747d65b9c5d73238929fb98eb7b5c0
Nguyễn Anh Tuấn <tuanna@hanu.edu.vn>
2025-02-05 17:37:23 +0700

    delete all
    delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# public
-For public
--- websocket.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ask for password securely
-read -s -p "Enter decryption password: " SCRIPT_PASS
-echo
-
-# Download and decrypt the encrypted script, then execute it
-curl -s "https://raw.githubusercontent.com/tuanna-git/public/main/websocket.sh" | openssl enc -aes-256-cbc -d -pbkdf2 -salt -pass pass:"$SCRIPT_PASS" | bash
